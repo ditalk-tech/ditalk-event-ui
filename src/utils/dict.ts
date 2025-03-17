d8a395bfd1c535db12dccad3e9696c40f822c7c5
疯狂的狮子Li <15040126243@163.com>
2025-03-17 09:36:32 +0800

    update 优化 简化代码

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -7,21 +7,20 @@ export const useDict = (...args: string[]): { [key: string]: DictDataOption[] } 
   const res = ref<{
     [key: string]: DictDataOption[];
   }>({});
-  return (() => {
-    args.forEach(async (dictType) => {
-      res.value[dictType] = [];
-      const dicts = useDictStore().getDict(dictType);
-      if (dicts) {
-        res.value[dictType] = dicts;
-      } else {
-        await getDicts(dictType).then((resp) => {
-          res.value[dictType] = resp.data.map(
-            (p): DictDataOption => ({ label: p.dictLabel, value: p.dictValue, elTagType: p.listClass, elTagClass: p.cssClass })
-          );
-          useDictStore().setDict(dictType, res.value[dictType]);
-        });
-      }
-    });
-    return res.value;
-  })();
+
+  args.forEach(async (dictType) => {
+    res.value[dictType] = [];
+    const dicts = useDictStore().getDict(dictType);
+    if (dicts) {
+      res.value[dictType] = dicts;
+    } else {
+      await getDicts(dictType).then((resp) => {
+        res.value[dictType] = resp.data.map(
+          (p): DictDataOption => ({ label: p.dictLabel, value: p.dictValue, elTagType: p.listClass, elTagClass: p.cssClass })
+        );
+        useDictStore().setDict(dictType, res.value[dictType]);
+      });
+    }
+  });
+  return res.value;
 };
